9dae94ae6f7fdff1f623a539061bff8f841b88de
deaxparadox <nitish136900kushwaha@gmail.com>
2025-02-04 14:53:30 +0530

    hello flask

--- hello/main.py
+++ hello/main.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.get("/")
 def hello_world():
-    return "<p>Hello, world!</p>"
+    app.logger.info("Hello to flask")
+    return render_template("index.html")
+
+@app.get("/<name>")
+def hello_name(name = None):
+    app.logger.info(f"Hello to {name}")
+    return render_template("index_name.html", name=name)
